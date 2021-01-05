0d27f381c830d3116850fe50f7094d0021009db4
Mateusz Matejczyk <mmat@google.com>
2021-01-06 00:36:06 +0100

    Improve assert methods

--- assert/assert.go
+++ assert/assert.go
@@ -1,16 +1,54 @@
 // Package assert defines the family of "assert" functions.
 package assert
 
-import "fmt"
+import (
+	"fmt"
+	"runtime"
+)
 
-func Assert(condition bool) {
+func That(condition bool, fmtAndArgs ...interface{}) {
 	if !condition {
-		panic("Condition is not true")
+		panic(newAssertError("condition is not true", fmtAndArgs))
 	}
 }
 
-func Must(err error) {
+func NoError(err error, fmtAndArgs ...interface{}) {
 	if err != nil {
-		panic(fmt.Sprintf("Error is not nil: %v", err))
+		panic(newAssertError("error is not nil: "+err.Error(), fmtAndArgs))
 	}
+}
+
+func errorMsg(defaultMsg string, fmtAndArgs ...interface{}) string {
+	if len(fmtAndArgs) == 0 {
+		return defaultMsg
+	} else {
+		return fmt.Sprintf(fmtAndArgs[0].(string), fmtAndArgs[1:]...)
+	}
+}
+
+func newAssertError(defaultMsg string, fmtAndArgs ...interface{}) *assertError {
+	return &assertError{
+		method: methodName(),
+		msg:    errorMsg(defaultMsg, fmtAndArgs),
+	}
+}
+
+type assertError struct {
+	method string
+	msg    string
+}
+
+func (e *assertError) Error() string {
+	return fmt.Sprintf("assert: assertion failed in %s: %s", e.method, e.msg)
+}
+
+// methodName returns the name of the calling method, assumed to be three stack
+// frames above.
+func methodName() string {
+	pc, _, _, _ := runtime.Caller(3)
+	f := runtime.FuncForPC(pc)
+	if f == nil {
+		return "unknown method"
+	}
+	return f.Name()
 }
